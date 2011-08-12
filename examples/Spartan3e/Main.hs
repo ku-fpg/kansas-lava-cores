0578670798a561eab4d2143ae40b4fe55c5d1f07
Andrew Gill <andy@dhcp248.ittc.ku.edu>
2011-08-12 15:52:39 -0500

    Fixing Spartan3e example to work with new lava.

--- examples/Spartan3e/Main.hs
+++ examples/Spartan3e/Main.hs
@@ -5,10 +5,11 @@ import Hardware.KansasLava.RS232
 import Hardware.KansasLava.FIFO
 import Data.Sized.Ix
 import Data.Sized.Unsigned
+import Data.Sized.Arith
 
 type X512 = ADD X256 X256
 type X1024 = ADD X512 X512
-
+type X16K   = MUL X1024 X16
 
 --rs232in >==> fifoZ >~~> bridge >==> rs232out
 
@@ -17,15 +18,18 @@ fabric = do
         in_rs  <- inStdLogic "rs_in" 
         let in_val                     = rs232in baudRate clockRate in_rs
 	    rs232in_patch	       = enableToHandShake in_val
-	    fifo_patch		       = fifo (Witness :: Witness X100) low 
+	    fifo_patch		       = fifo (Witness :: Witness X16) low 
 	    rs232out_patch	       = rs232out (baudRate {- + 2000 -}) clockRate 
 	    main_patch		       = rs232in_patch >~=> fifo_patch >~=> rs232out_patch
+
 	    ((),_ :> counter :> out_rs,())  = main_patch ((),())
 
         outStdLogic "rs_out" (out_rs :: Seq Bool)
 ----        outStdLogicVector "leds" (latch out_val)
-        outStdLogicVector "leds" ((unsigned) counter :: Seq U8)
-	return ()		
+        outStdLogicVector "leds" $ 
+--			((unsigned) counter :: Seq U8)
+			(0xa6 :: Seq U8)
+	return ()
 
 baudRate = 115200
 --baudRate   = 117000
