188203a1120214c0bfa52e20e6a77b50fa717138
Andrew Gill <andy@2024-andygill-3.eecs.ku.edu>
2011-09-07 14:35:31 -0500

    Cleanup for the Spartan3e file, adding haddock docs.

--- Hardware/KansasLava/Boards/Spartan3e.hs
+++ Hardware/KansasLava/Boards/Spartan3e.hs
@@ -1,4 +1,16 @@
-module Hardware.KansasLava.Boards.Spartan3e where
+module Hardware.KansasLava.Boards.Spartan3e (
+	-- * Initialization, and global settings.
+	  board_init
+	, rot_as_reset
+	, clockRate
+	, showUCF
+	-- * Patch API's.
+	, lcdPatch
+	, switchesPatch
+	-- * Raw API's.
+	, lcd
+	, switches
+	) where
 
 import Language.KansasLava as KL
 import Hardware.KansasLava.LCD.ST7066U
@@ -6,6 +18,34 @@ import Data.Sized.Unsigned
 import Data.Sized.Ix
 import Data.Sized.Matrix
 
+------------------------------------------------------------
+-- initialization
+------------------------------------------------------------
+
+-- | 'board_init' sets up the use of the clock. Required.
+board_init :: Fabric ()
+board_init = do
+	theClk "CLK_50MHZ"		-- we need to name and pull in the clock
+
+-- | 'rot_as_reset' sets up the rotary dial as a reset switch.
+rot_as_reset :: Fabric ()
+rot_as_reset = theRst "ROT_CENTER"
+
+-- | The clock rate on the Spartan3e (50MHz), in hertz.
+clockRate :: Integer
+clockRate = 50 * 1000 * 1000
+
+-- | show out a suggested UCF file for Spartan3e, for a specific circuit.
+showUCF :: KLEG -> String
+showUCF _ = "# TODO"
+
+-- The UCF file, in Haskell list format.
+ucf :: [(String,String)]
+ucf = []
+
+------------------------------------------------------------
+-- Patches
+------------------------------------------------------------
 -- 
 -- | 'lcdPatch' gives a patch-level API to the LCD, based on LCDInstructions.
 --  Disables the StrataFlash (for now).
@@ -18,6 +58,18 @@ lcdPatch =
 		let (rs,sf_d,e) = unpack bus
 		lcd rs sf_d e)
 
+-- | 'switchesPatch' gives a patch-level API for the toggle switches.
+
+switchesPatch :: Fabric (Patch () (Matrix X4 (Seq Bool))
+			       () (Matrix X4 ()))
+switchesPatch = do
+	sws <- switches
+	return (unitPatch sws $$ backwardPatch (\ _mat -> ()))
+
+------------------------------------------------------------
+-- RAW APIs
+------------------------------------------------------------
+
 -- | 'lcd' give raw access to the lcd bus. Disables the StrataFlash (for now).
 
 lcd :: Seq U1 -> Seq U4 -> Seq Bool -> Fabric ()
@@ -28,23 +80,8 @@ lcd rs sf_d e = do
 		outStdLogic "LCD_RW" low
 		outStdLogic "SF_CE0" high
 
--- | 'switchesPatch' gives a patch-level API for the toggle switches.
-
-switchesPatch :: Fabric (Patch () (Matrix X4 (Seq Bool))
-			       () (Matrix X4 ()))
-switchesPatch = do
-	sws <- switches
-	return (unitPatch sws $$ backwardPatch (\ _mat -> ()))
 
 -- | 'switches' gives raw access to the position of the toggle switches.
 switches :: Fabric (Matrix X4 (Seq Bool))
 switches = undefined
 
--- | 'board_init' sets up the use of the clock. Required.
-board_init :: Fabric ()
-board_init = do
-	theClk "CLK_50MHZ"		-- we need to name and pull in the clock
-
--- | 'rot_as_reset' sets up the rotary dial as a reset switch.
-rot_as_reset :: Fabric ()
-rot_as_reset = theRst "ROT_CENTER"
