996288d9b1ba6f15d2a7ef531b6b3434a951cf2d
Andy Gill <andygill@ku.edu>
2011-12-10 20:46:19 -0600

    Adding a new class - PhysicalMonad, for physical board.

---
+++ Hardware/KansasLava/Boards/Physical.hs
@@ -0,0 +1,10 @@
+module Hardware.KansasLava.Boards.Physical where
+
+import Language.KansasLava
+
+class Monad m => PhysicalMonad m where
+  run_physical :: m a -> STMT a
+
+  clk_physical :: m String
+  clk_physical = return "clk"
+
